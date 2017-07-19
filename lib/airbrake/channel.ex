1f23281f20f89578569744c625f99c3cab06a48e
Nick Ewing <nick@nickewing.net>
2017-07-19 11:49:30 -0700

    Wrap channel messages in map

--- lib/airbrake/channel.ex
+++ lib/airbrake/channel.ex
@@ -15,7 +15,7 @@ defmodule Airbrake.Channel do
           super(channel_name, msg, socket)
         rescue
           exception ->
-            send_to_airbrake(exception, socket.assigns, msg, %{channel: channel_name})
+            send_to_airbrake(exception, socket.assigns, %{message: msg}, %{channel: channel_name})
         end
       end
 
@@ -24,7 +24,7 @@ defmodule Airbrake.Channel do
           super(msg_type, msg, socket)
         rescue
           exception ->
-            send_to_airbrake(exception, socket.assigns, msg, %{msg_type: msg_type})
+            send_to_airbrake(exception, socket.assigns, %{message: msg}, %{msg_type: msg_type})
         end
       end
 
@@ -33,7 +33,7 @@ defmodule Airbrake.Channel do
           super(msg, socket)
         rescue
           exception ->
-            send_to_airbrake(exception, socket.assigns, msg)
+            send_to_airbrake(exception, socket.assigns, %{message: msg})
         end
       end
 
